d068288db32217c42f8dc9f339062f1b379ccb04
Victor Dolsan <victorddf5@gmail.com>
2021-12-27 20:21:03 -0300

    Inicializaçao da Secao 7 do Curso da Udemy de C#

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace CSharpSecaoSete
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            
+        }
+    }
+}
